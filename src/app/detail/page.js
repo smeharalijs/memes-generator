1ed14883ff96a9182afb3e611de9ce56eb415eaa
Syed Mehar Ali Shah <syedzada12550@gmail.com>
2024-02-17 01:13:27 +0500

    additional work

--- src/app/detail/page.js
+++ src/app/detail/page.js
@@ -1,45 +1,52 @@
-// import Link from "next/link"
+"use client"
+import { useState, createRef } from "react"
+import { useSearchParams } from "next/navigation"
+import Draggable from "react-draggable"
+import { exportComponentAsJPEG } from "react-component-export-image"
 
-// async function Home() {
-//   const res = await fetch('https://api.imgflip.com/get_memes')
-//   const response = await res.json()
+const memeRef = createRef()
+const exportMeme = () => {
+    exportComponentAsJPEG(memeRef, {
+        fileName: 'meme'
+    })
+}
 
-//   return (
-//     <div>
-//       <Link href="home">Home</Link>
-//       <Link href="detail">Detail</Link>
+function Detail() {
+    const searchParams = useSearchParams()
+    const Url = searchParams.get('url')
+    const [input1, setInput1] = useState('');
+    const [input2, setInput2] = useState('');
 
-//       <div>
-//         {response.data.memes.map((item, index) => {
-//           return (
-//             <div className="max-w-sm w-full lg:max-w-full lg:flex" key={index}>
-//               <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style={{backgroundImage: `url('${item.url}')`}} title="Woman holding a mug">
-//               </div>
-//               <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-//                 <div className="mb-8">
-//                   <p className="text-sm text-gray-600 flex items-center">
-//                     <svg className="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-//                       <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
-//                     </svg>
-//                     Members only
-//                   </p>
-//                   <div className="text-gray-900 font-bold text-xl mb-2">Can coffee make you a better developer?</div>
-//                   <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
-//                 </div>
-//                 <div className="flex items-center">
-//                   <img className="w-10 h-10 rounded-full mr-4" src="" alt="Avatar of Jonathan Reinink"/>
-//                   <div className="text-sm">
-//                     <p className="text-gray-900 leading-none">Jonathan Reinink</p>
-//                     <p className="text-gray-600">Aug 18</p>
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//           )
-//         })}
-//       </div>
-//     </div>
-//   )
-// }
+    return (
+        <div className="flex flex-wrap justify-center">
+            <div className="w-64 rounded overflow-hidden shadow-lg mx-4 my-4">
 
-// export default Home
+
+
+
+                <div ref={memeRef} className="w-[100%]" >
+
+                    <img className="w-[100%]" src={Url} alt="img" />
+                    <div className="px-6 py-4">
+
+                        <Draggable>
+                            <div className="font-bold text-xl mb-2">{input1}</div>
+                        </Draggable>
+
+
+                        <Draggable>
+                            <div className="font-bold text-xl mb-2">{input2}</div>
+                        </Draggable>
+                    </div>
+                    </div>
+                    <input className="border-2" type="text" onChange={event => setInput1(event.target.value)} placeholder="Text #1" />
+                    <input className="border-2" type="text" onChange={event => setInput2(event.target.value)} placeholder="Text #2" />
+                    <div onClick={exportMeme} className="p-1 rounded-md w-[100%] bg-gray-700 text-white hover:bg-white hover:text-black">
+                        Generate Meme
+                    </div>
+                </div>
+            </div>
+    )
+}
+
+export default Detail
--- src/app/page.js
+++ src/app/page.js
@@ -3,28 +3,26 @@ import Link from "next/link"
 async function Home() {
   const res = await fetch('https://api.imgflip.com/get_memes')
   const response = await res.json()
+  console.log(response.data.memes);
 
   return (
     <div>
-      <Link href="home">Home</Link>
-      <Link href="detail">Detail</Link>
+  <h1 className="text-center bg-white w-[100%]">Memes Generator</h1>
 
       <div className="flex flex-wrap justify-center">
         {response.data.memes.map((item, index) => (
           <div className="w-64 rounded overflow-hidden shadow-lg mx-4 my-4" key={index}>
-            {/* <img className="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains"> */}
             <img className="w-full" src={item.url} alt="img"/>
             <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+              <div className="font-bold text-xl mb-2">Id : {item.id}</div>
               <p className="text-gray-700 text-base">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                Name : {item.name}
               </p>
+              <div className="p-1 rounded-md w-[50%] bg-white hover:bg-black hover:text-white">
+              <Link href={`detail?url=${item.url}`}>Open Meme</Link>
+              </div>
             </div>
-            <div className="px-6 pt-4 pb-2">
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-            </div>
+          
           </div>
         ))}
       </div>
